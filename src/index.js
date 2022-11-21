cbb6b2194fd652706f163151cb0a843c92a23025
dk7696822 <dk7696822@gmail.com>
2022-11-21 12:28:34 +0530

    Index file created

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const route = require("./route/route");
+app.use(express.json());
+mongoose
+  .connect(
+    "mongodb+srv://dk7696822:wnyQpzrA3d4AcykC@cluster0.hef809l.mongodb.net/Group7Database?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.log(err.message));
+app.use("/", route);
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
